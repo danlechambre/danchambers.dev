fc491240d92bdf7988a1e892c8465df30a543114
Dan Chambers <danlechambre@icloud.com>
2023-04-22 22:59:03 +0100

    Using Gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `danchambers.dev`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import MainLayout from '../layouts/MainLayout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const pageTitle = "Home Page";
 
@@ -7,6 +8,10 @@ const IndexPage = () => {
   return (
     <MainLayout pageTitle={pageTitle}>
       <p>Gatsby site y'all</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/clifford.jpg"
+      />
     </MainLayout>
   )
 }
