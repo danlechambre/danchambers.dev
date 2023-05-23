3959b682b4731fa6728ab086fc6b522a02decdf5
Dan Chambers <danlechambre@icloud.com>
2023-05-23 23:17:45 +0100

    Bit of content rework on home and about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Seo from "@components/Seo";
 import MainLayout from "@components/MainLayout/MainLayout";
+import AboutContent from "@content/about.mdx";
 
 const pageTitle = "About";
 
 const AboutPage = () => {
   return (
     <MainLayout pageTitle={pageTitle}>
-      <p>{`coming soon y'all`}</p>
+      <div className="prose">
+        <AboutContent />
+      </div>
     </MainLayout>
   );
 };
