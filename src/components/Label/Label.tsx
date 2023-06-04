8279d6d3157d89bf231b4e98cf1934bc3dc64020
Dan Chambers <danlechambre@icloud.com>
2023-06-04 23:29:00 +0100

    Changes to Label component for increased usability

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -1,18 +1,27 @@
 import React from "react";
-import { label } from "./Label.module.css";
+import { label, truncateTxt } from "./Label.module.css";
 import { ThemeVariant } from "shared-types";
 import { bgContrastColorClassNameMap, bgVariantClassNameMap } from "maps";
 
 interface LabelProps {
   children: React.ReactNode;
   variant?: ThemeVariant;
+  truncate?: boolean;
 }
 
-const Label = ({ children, variant = "neutral" }: LabelProps) => {
+const Label = ({ children, variant = "neutral", truncate }: LabelProps) => {
   const bgColor = bgVariantClassNameMap[variant];
   const color = bgContrastColorClassNameMap[variant];
 
-  return <span className={`${label} ${bgColor} ${color}`}>{children}</span>;
+  return (
+    <span
+      className={`font-sm font-bold ${label} ${bgColor} ${color} ${
+        truncate && truncateTxt
+      }`}
+    >
+      {children}
+    </span>
+  );
 };
 
 export default Label;
