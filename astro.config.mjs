34dbee65fe8680c4dc8c36c252fb34c0175a9cf1
Dan Chambers <danlechambre@icloud.com>
2026-05-09 21:17:47 +0100

    setup config and required pkgs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,39 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  fonts: [
+    {
+      name: "Inter",
+      cssVariable: "--font-sans",
+      provider: fontProviders.local(),
+      options: {
+        variants: [
+          {
+            weight: "100 900",
+            style: "normal",
+            src: ["./src/assets/fonts/inter-var.woff2"],
+          },
+        ],
+      },
+    },
+    {
+      name: "Lora",
+      cssVariable: "--font-serif",
+      provider: fontProviders.local(),
+      options: {
+        variants: [
+          {
+            weight: "100 900",
+            style: "normal",
+            src: ["./src/assets/fonts/lora-var.woff2"],
+          },
+          {
+            weight: "100 900",
+            style: "italic",
+            src: ["./src/assets/fonts/lora-italic-var.woff2"],
+          },
+        ],
+      },
+    },
+  ],
+});
