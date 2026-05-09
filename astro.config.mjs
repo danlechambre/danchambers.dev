e4d226e79b0ea4807bb12464a0aca348838b6d1e
Dan Chambers <danlechambre@icloud.com>
2026-05-09 22:14:07 +0100

    configure for cf deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig, fontProviders } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   fonts: [
     {
@@ -36,4 +38,6 @@ export default defineConfig({
       },
     },
   ],
+
+  adapter: cloudflare(),
 });
